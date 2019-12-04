8263559ee2306168a36b942f2cd4ce646c7017d3
William DeLaughter <williamd.delaughter@gmail.com>
2019-12-04 11:06:21 -0600

    New branch because didn't want to fight merge

---
+++ 04-test-index-add.py
@@ -0,0 +1,71 @@
+#!/bin/python
+from file import *
+from file.index import IndexFile
+from file.paging import INVALID_OFF
+from file.valuetype import Float32
+
+types = [vt.TEXT]
+
+#Tag ID     Name     Weight(kg)    Age (years)
+data = ((933,   b'Rover',   Float32(20.6), 4),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (8326,  b'Spot',    Float32(10.8), 7),
+        (5359,  b'Lucky',   Float32(31.2), 5),
+        (10355, b'Dinky',   Float32(4.8),  11),
+        (7757,  b'Bruiser', Float32(42.0), 6), 
+        (3597,  b'Patch',   Float32(29.6), 9), 
+        (202,   b'Prince',  Float32(16.6), 7), 
+        (5359,  b'Lucky',   Float32(31.2), 5),
+        (5359,  b'Lucky',   Float32(31.2), 5),
+        (5359,  b'Lucky',   Float32(31.2), 5),
+        (10355, b'Dinky',   Float32(4.8),  11),
+        (7757,  b'Bruiser', Float32(42.0), 6), 
+        (3597,  b'Patch',   Float32(29.6), 9), 
+        (202,   b'Prince',  Float32(16.6), 7), 
+        (10355, b'Dinky',   Float32(4.8),  11),
+        (7757,  b'Bruiser', Float32(42.0), 6), 
+        (3597,  b'Patch',   Float32(29.6), 9), 
+        (202,   b'Prince',  Float32(16.6), 7), 
+        (10355, b'Dinky',   Float32(4.8),  11),
+        (7757,  b'Bruiser', Float32(42.0), 6), 
+        (3597,  b'Patch',   Float32(29.6), 9), 
+        (202,   b'Prince',  Float32(16.6), 7), 
+        (1630,  b'Bubbles', Float32(7.1),  11),
+        (1223,  b'Peanut',  Float32(14.3), 2))
+'''
+data = ((933,   b'Rover',   Float32(20.6), 4),
+        (8326,  b'Spot',    Float32(10.8), 7), 
+        (5359,  b'Lucky',   Float32(31.2), 5), 
+        (10355, b'Dinky',   Float32(4.8),  11),
+        (7757,  b'Bruiser', Float32(42.0), 6), 
+        (3597,  b'Patch',   Float32(29.6), 9), 
+        (202,   b'Prince',  Float32(16.6), 7), 
+        (1630,  b'Bubbles', Float32(7.1),  11),
+        (1223,  b'Peanut',  Float32(14.3), 2))
+'''
+dbfile = IndexFile(open('dogs.ind', 'w+b'), types, 128, root_page = INVALID_OFF)
+dbfile.add(88,b'Rover')
+for j in range(dbfile.next_page()):
+    print(repr(dbfile.read_page(j)))
+input('************* Next... *****************')
+#REMOVED LOOP FOR NOW 
+for i in range(len(data)):
+
+    dbfile.add(i,data[i][1])
+    for j in range(dbfile.next_page()):
+        print(repr(dbfile.read_page(j)))
+    input('************* Next... *****************')
+
+
